a07d01b1bfde2649f84d5ba52c03e54982170d06
unknown <Moussa@.(none)>
2013-10-12 15:18:54 -0400

    first commit

---
+++ page.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<LINK rel=stylesheet type="text/css" href="css/style.css">
+<link rel="shortcut icon" href="../favicon.ico"> 
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/slide.css" />
+		<!--[if lt IE 9]>
+		<link rel="stylesheet" type="text/css" href="css/slide_ie.css" />
+		<![endif]-->
+<link rel="stylesheet" href="css/menu.css" type="text/css" media="screen" charset="utf-8"/>
+       <SCRIPT language="JavaScript" SRC="./js/IE-fix.js"></SCRIPT>
+		<!-- jQuery -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<!-- jmpress plugin -->
+		<script type="text/javascript" src="js/jmpress.min.js"></script>
+		<!-- jmslideshow plugin : extends the jmpress plugin -->
+		<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
+		<script type="text/javascript" src="js/modernizr.custom.48780.js"></script>
+<!-- jQuery -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<!-- jmpress plugin -->
+		<script type="text/javascript" src="js/jmpress.min.js"></script>
+		<!-- jmslideshow plugin : extends the jmpress plugin -->
+		<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
+		<script type="text/javascript" src="js/modernizr.custom.48780.js"></script>
+<title>BinarySolution</title>
+</head>
+
+<body>
+<div class="page">
+	<header>
+		<div class="slide"><?php include './pages/slide.php' ?></div>
+		<div class="menutop"><?php include './pages/menu.php' ?></div>
+	</header>
+	<nav class="menuside">Maintenance</nav>
+    <div class="content">
+    Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.
+
+Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+
+Sed laeditur hic coetuum magnificus splendor levitate paucorum incondita, ubi nati sunt non reputantium, sed tamquam indulta licentia vitiis ad errores lapsorum ac lasciviam. ut enim Simonides lyricus docet, beate perfecta ratione vieturo ante alia patriam esse convenit gloriosam.
+
+Non ergo erunt homines deliciis diffluentes audiendi, si quando de amicitia, quam nec usu nec ratione habent cognitam, disputabunt. Nam quis est, pro deorum fidem atque hominum! qui velit, ut neque diligat quemquam nec ipse ab ullo diligatur, circumfluere omnibus copiis atque in omnium rerum abundantia vivere? Haec enim est tyrannorum vita nimirum, in qua nulla fides, nulla caritas, nulla stabilis benevolentiae potest esse fiducia, omnia semper suspecta atque sollicita, nullus locus amicitiae.
+    </div>
+	<footer>
+    <div class="menufooter">footer ici</div>
+    </footer>
+</div>
+</body>
+
+</html> 
---
+++ pages/menu.php
@@ -0,0 +1,10 @@
+
+	<div id="nav">
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#" id="active">Services</a></li>
+		 	<li><a href="#">A propos de nous</a></li>
+			<li><a href="#">Porte Folio</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
