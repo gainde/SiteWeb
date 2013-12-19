7b2b3f3f2f91611bcd4b7e43a61a35c10d12140f
Moussa Thimbo <moussa.thimbo@gmail.com>
2013-12-19 15:56:13 -0500

    ajout index

--- index.php
+++ index.php
@@ -6,48 +6,61 @@
 <link rel="shortcut icon" href="../favicon.ico"> 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="css/slide.css" />
+<link rel="stylesheet" href="pages/calendrier/css/eventCalendar.css">
+
+	<!-- Theme CSS file: it makes eventCalendar nicer -->
+	<link rel="stylesheet" href="pages/calendrier/css/eventCalendar_theme_responsive.css">
+		<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+		<noscript>
+			<link rel="stylesheet" type="text/css" href="css/nojs.css" />
+		</noscript>
 		<!--[if lt IE 9]>
 		<link rel="stylesheet" type="text/css" href="css/slide_ie.css" />
 		<![endif]-->
 <link rel="stylesheet" href="css/menu.css" type="text/css" media="screen" charset="utf-8"/>
-       <SCRIPT language="JavaScript" SRC="./js/IE-fix.js"></SCRIPT>
+<link rel='stylesheet' type='text/css' href='css/menuh.css' />
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+
+<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>-->
+<!--<script src="http://code.jquery.com/jquery-1.9.1.js"></script>-->
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+<script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
+<!--  SLIDER SCRIPTS -->
+<script type="text/javascript" src="js/jquery.cslider.js"></script>
+<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
+
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<!-- jmslideshow plugin : extends the jmpress plugin -->
+		
+
+	<!--<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>-->
+<script type='text/javascript' src='js/menuh_jquery.js'></script>
+       <!--<SCRIPT language="JavaScript" SRC="./js/IE-fix.js"></SCRIPT>-->
 		<!-- jQuery -->
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-		<!-- jmpress plugin -->
-		<script type="text/javascript" src="js/jmpress.min.js"></script>
-		<!-- jmslideshow plugin : extends the jmpress plugin -->
-		<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
-		<script type="text/javascript" src="js/modernizr.custom.48780.js"></script>
-<!-- jQuery -->
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-		<!-- jmpress plugin -->
-		<script type="text/javascript" src="js/jmpress.min.js"></script>
-		<!-- jmslideshow plugin : extends the jmpress plugin -->
-		<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
-		<script type="text/javascript" src="js/modernizr.custom.48780.js"></script>
+<script src="pages/calendrier/js/jquery.eventCalendar.js" type="text/javascript"></script>		
+		
+
 <title>BinarySolution</title>
 </head>
 
 <body>
-<div class="page">
-	<header>
-		<div class="slide"><?php include './pages/slide.php' ?></div>
-		<div class="menutop"><?php include './pages/menu.php' ?></div>
+
+	<header class="top">
+		<div class="slide"><?php include './pages/slide.php' ?></div>	
 	</header>
-	<nav class="menuside">Maintenance</nav>
-    <div class="content">
-    Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.
-
-Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
-
-Sed laeditur hic coetuum magnificus splendor levitate paucorum incondita, ubi nati sunt non reputantium, sed tamquam indulta licentia vitiis ad errores lapsorum ac lasciviam. ut enim Simonides lyricus docet, beate perfecta ratione vieturo ante alia patriam esse convenit gloriosam.
-
-Non ergo erunt homines deliciis diffluentes audiendi, si quando de amicitia, quam nec usu nec ratione habent cognitam, disputabunt. Nam quis est, pro deorum fidem atque hominum! qui velit, ut neque diligat quemquam nec ipse ab ullo diligatur, circumfluere omnibus copiis atque in omnium rerum abundantia vivere? Haec enim est tyrannorum vita nimirum, in qua nulla fides, nulla caritas, nulla stabilis benevolentiae potest esse fiducia, omnia semper suspecta atque sollicita, nullus locus amicitiae.
-    </div>
+ 
+    <div class="page">
+    	<div class="menutop"><?php include './pages/menu.php' ?></div>
+		<div class="menuside">
+			<?php include './pages/menuh.php' ?>
+            <?php include './pages/calendrier/calendrier.php' ?>
+         </div>
+    	<div class="content"><?php include './services.php' ?></div>
+  </div>
 	<footer>
     <div class="menufooter">footer ici</div>
     </footer>
-</div>
+
 </body>
 
 </html> 
